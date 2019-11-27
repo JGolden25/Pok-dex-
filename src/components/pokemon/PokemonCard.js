039491c1add4851531e6d22917962d7d5b3246c0
Joseph Chretien-Golden <joechre@gmail.com>
2019-11-27 00:36:30 -0600

    showing a few cards/names on screen

--- src/components/pokemon/PokemonCard.js
+++ src/components/pokemon/PokemonCard.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react'
 
 export default class PokemonCard extends Component {
-
+    state ={
+        name: '',
+        imageUrl:'',
+        pokemonindex:''
+    };
     render(){
+        const { name, url } = this.props;
+       
+
         return(
         <div className='col-md-3 col-sm-6 mb-5'>
             <div className="card">
             <div className="card-header">
-                <h1>Joseph Golden</h1>
+                <h1>{name}</h1>
             </div>
             </div>
         </div>
--- src/components/pokemon/PokemonList.js
+++ src/components/pokemon/PokemonList.js
@@ -10,19 +10,27 @@ export default class PokemonList extends Component {
     };
 
 async componentDidMount(){
-    const res = axios.get(this.state.url);
+    const res = await axios.get(this.state.url);
     this.setState({pokemon: res.data['results']});
 }
 
     render() {
         return (
+            <React.Fragment>
+            {this.state.pokemon ? (
         <div className= "row">
-            <PokemonCard />
-            <PokemonCard />
-            <PokemonCard />
-            <PokemonCard />
-            <PokemonCard />
+           {this.state.pokemon.map(pokemon => (
+               <PokemonCard 
+               key={pokemon.name}
+                name={pokemon.name}
+                url={pokemon.irl}
+               />
+           ))}
         </div>
+        ) : (
+            <h1>Loading Pokemon</h1>
+        )}
+        </React.Fragment>
         );
     }
 }
